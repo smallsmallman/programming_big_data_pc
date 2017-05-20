7334c1491a482beeac53ee70360594e8137a2355
Paul Coleman <10355197@mydbs.ie>
2017-05-20 15:03:46 +0100

    final versions of python files and report

--- ca4_update.py
+++ ca4_update.py
@@ -1,45 +1,47 @@
-#this file uses dictionary as opposed to classes
-#this file doesn't bother getting the changes or comments, do this ourselves
+#this file uses dictionaries as opposed to classes
 changes_file = 'changes_python.log'
 
 def read_file(changes_file):
-	# use strip to strip out spaces and trim the line.
+	# strip us used to strip out spaces and trim the line.
 	data = [line.strip() for line in open(changes_file, 'r')]
 	return data
 
+#get the number of commitments made over the whole dataset
 def get_commits(data):
+	#each commitment is separated by 72 hyphons
 	sep = 72*'-'
 	commits = []
 	index = 0
 	while index < len(data):
 		try:
-			# parse each of the commits and put them into a list of commits
 			details = data[index + 1].split('|')
-			# the author with spaces at end removed.
+			#parsing each of the commits and putting them into a dictionary with the associated keys and values
 			commit = {'revision': details[0].strip(),
 				'author': details[1].strip(),
 				'date': details[2].strip(),
 				'number_of_lines': details[3].strip().split(' ')[1]
 			}
-			# add details to the list of commits.
+			#appending each commit to the list 'commits'
 			commits.append(commit)
 			index = data.index(sep, index + 1)
 		except IndexError:
 			break
 	return commits
 
-
+#get a list of which authors made commits and how many each made
 def get_authors(data):
 	sep = 72*'-'
 	authors = {}
 	index = 0
 	while index < len(data):
 		try:
-			# parse each of the authors and put them into a list of authors
+			#finding the authors within each commit
 			author = data[index + 1].split('|')[1].strip()
+			#adding new authors to the dictionary of authors, with an assignment of 1 commitment
 			if author not in authors:
 				authors[author] = 1
 				index += 1
+			#adding an additional value of 1 every time the author is found
 			else:
 				authors[author] += 1
 			index = data.index(sep, index + 1)
@@ -47,7 +49,7 @@ def get_authors(data):
 			break
 	return authors
 
-	
+#similar to authors, finding which day produces the most commitments
 def get_busy_day(data):
 	sep = 72*'-'
 	days = {}
@@ -66,44 +68,47 @@ def get_busy_day(data):
 			break
 	return days
 
-
-def get_quantity_files(data):
+#similar to 'get_busy_day' finding which month produces the most commitments
+def get_busy_month(data):
 	sep = 72*'-'
-	file_count = {}
+	months = {}
 	index = 0
 	while index < len(data):
 		try:
-			details = data[index + 1].split('|')
-			# parse each of the commits and put them into a list of commits
-			# the author with spaces at end removed.
-			file_count = {'Author': details[1].strip(), 'Changes': data[index+2:data.index('',index+1)]}
+			date = data[index + 1].split('|')[2].strip()
+			month = date.split('(')[1].split(' ')[2]
+			if month not in months:
+				months[month] = 1
+				index += 1
+			else:
+				months[month] += 1
 			index = data.index(sep, index + 1)
 		except IndexError:
 			break
-	return file_count
-	
-		
-if __name__ == '__main__':
-	# open the file - and read all of the lines.
-	changes_file = 'changes_python.log'
-	data = read_file(changes_file)
-	commits = get_commits(data)
-	authors = get_authors(data)
-	days = get_busy_day(data)
-	files = get_quantity_files(data)
-	
+	return months	
 
-# print the number of lines read
-	print 'Number of lines of data: ', len(data)
-	print 'Number of commitments: ', len(commits)
-	print 'Authors: ', authors
-	print 'Days: ', days
-	print 'File count per author: ', len(files)
 
+#the following two functions were attempted but due to time constraints could be completed at a later date
 	
-	
-	
-	#def get_busy_timeofday(data):
+#def get_quantity_files(data):
+#	sep = 72*'-'
+#	file_count = []
+#	index = 0
+#	while index < len(data):
+#		try:
+#			#details = data[index + 1].split('|')
+#			# parse each of the commits and put them into a list of commits
+#			# the author with spaces at end removed.
+#			files = data[index+2:data.index('',index+1)]
+#			for line in files.split('\n'):
+#				file_count.append(files)
+#			index = data.index(sep, index + 1)
+#		except IndexError:	
+#			break
+#	return file_count
+
+
+#def get_busy_timeofday(data):
 #	sep = 72*'-'
 #	morning = {}
 #	afternoon = {}
@@ -126,9 +131,21 @@ if __name__ == '__main__':
 #		except IndexError:
 #			break
 #	return morning, 'xxxxx', afternoon, 'xxxxx', unsociable
+	
 
-#print(commits)
-#print commits
 
-#print(commits[1]['author'])
-#print(len(commits))
+		
+if __name__ == '__main__':
+	#testing the functions
+	changes_file = 'changes_python.log'
+	data = read_file(changes_file)
+	commits = get_commits(data)
+	authors = get_authors(data)
+	days = get_busy_day(data)
+	months = get_busy_month(data)
+
+	print 'Number of lines of data: ', len(data)
+	print 'Number of commitments: ', len(commits)
+	print 'Authors and associated commits: ', authors
+	print 'Days and associated commits: ', days
+	print 'Months and associated commits: ', months
